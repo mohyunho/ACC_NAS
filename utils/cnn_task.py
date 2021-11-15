35c295aac84bb65c074cb136b9fdbfc1d19dadde
hyunho.mo <hyunho.mo@unitn.it>
2021-11-15 13:50:51 +0100

    lr

--- utils/cnn_task.py
+++ utils/cnn_task.py
@@ -71,7 +71,7 @@ class SimpleNeuroEvolutionTask(Task):
         n_filters = genotype[1]
         kernel_size = genotype[2]
         n_mlp = 10 * genotype[3]
-        lr = 10 ^ (-int(genotype[4]))
+        lr = 10**(-1*genotype[4])
         # lr_lst = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
         # lr= lr_lst[genotype[4]-1]
 
